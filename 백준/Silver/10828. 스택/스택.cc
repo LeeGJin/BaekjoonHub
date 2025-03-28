cb9e9cc71a68eb3a3810598fc2e1d60d63289cad
LeeGyeongJin <78292636+LeeGJin@users.noreply.github.com>
2025-03-29 03:48:18 +0900

    [Silver IV] Title: 스택, Time: 24 ms, Memory: 98688 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 10000
+
+typedef struct {
+    int stack[MAX_STACK_SIZE];
+    int top;
+}StackType;
+
+void init_stack(StackType* s) {
+    s->top = -1;
+}
+
+void push(StackType* s, int item) {
+    s->stack[++(s->top)] = item;
+    return;
+}
+
+int empty(StackType* s) {
+    return (s->top == -1);
+}
+
+int pop(StackType* s) {
+    if (empty(s)) {
+        return -1;
+    }
+    return (s->stack[(s->top)--]);
+}
+
+int size(StackType* s) {
+    return (s->top+1);
+}
+
+int top(StackType* s) {
+    if (empty(s)) {
+        return -1;
+    }
+    return (s->stack[s->top]);
+}
+
+int main() {
+    StackType stack;
+    init_stack(&stack);
+    int num, val;
+    scanf("%d", &num);
+
+    char arr[num][10000];
+
+    for (int i = 0; i < num; i++) {
+        scanf(" %[^\n]s", arr[i]);
+    }
+    
+    for (int j = 0; j < num; j++) {
+        if (strncmp(arr[j], "push", 4) == 0) {
+            // "push" 명령 처리 
+            //strncmp(str1, str2, n) str1과 str2를 최대 n개의 문자만 비교
+            //0이면 일치
+            sscanf(arr[j], "push %d", &val); 
+            // "push 1"과 같은 형식을 처리, 숫자 추출
+            push(&stack, val);
+        } 
+        else if (strcmp(arr[j], "pop") == 0) {
+            printf("%d\n", pop(&stack));
+        } 
+        else if (strcmp(arr[j], "size") == 0) {
+            printf("%d\n", size(&stack));
+        } 
+        else if (strcmp(arr[j], "empty") == 0) {
+            printf("%d\n", empty(&stack));
+        } 
+        else if (strcmp(arr[j], "top") == 0) {
+            printf("%d\n", top(&stack));
+        }
+    }
+
+    return 0;
+}
